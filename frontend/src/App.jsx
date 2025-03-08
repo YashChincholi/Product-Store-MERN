5d9953910b9f0a832d5932ab900ce81fc4dfad33
YashChincholi <Yashchincholi18@gmail.com>
2025-03-08 07:42:54 +0530

    Created Navbar with dark light mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,11 @@ import { Box } from "@chakra-ui/react";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import Navbar from "./components/Navbar";
+import { useColorModeValue } from "./components/ui/color-mode";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.200", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { BsPlusSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useColorMode } from "./ui/color-mode";
+import { LuSun } from "react-icons/lu";
+import { IoMoon } from "react-icons/io5";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        direction={{
+          base: "column",
+          sm: "row",
+        }}
+        justify={"space-between"}
+        align={"center"}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient="to-r"
+          gradientFrom="cyan.400"
+          gradientTo="blue.500"
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
 
-export default Navbar
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <BsPlusSquare size={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode ? <LuSun /> : <IoMoon />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
