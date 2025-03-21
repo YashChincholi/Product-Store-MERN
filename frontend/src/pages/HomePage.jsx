9cd8c357662ca8ef6a8d9b72b9e9620f62b6111c
YashChincholi <Yashchincholi18@gmail.com>
2025-03-21 17:03:49 +0530

    Created fetch data in productstore

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import { useProductStore } from "@/store/product";
 import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log(products);
+
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
